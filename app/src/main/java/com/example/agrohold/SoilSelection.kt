7fc1fa5788970522df9ed05e4c0daf00be83c406
vladihka <vladihka58@gmail.com>
2023-04-17 22:13:04 +0200

    SoilSelection
    Adding the next window SoilSelection

---
+++ app/src/main/java/com/example/agrohold/SoilSelection.kt
@@ -0,0 +1,44 @@
+package com.example.healthyharvest
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatActivity
+
+class SoilSelection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_soil_selection)
+
+        // val soilSubmit = findViewById<Button>(R.id.soil_submit_button)
+        val soilSelectionRadioGrp = findViewById<RadioGroup>(R.id.soil_selection_radio_grp)
+        /* val demoText = findViewById<TextView>(R.id.Demo_Text) */
+
+        supportActionBar?.title = "Step 2 : Soil Type ☘"
+
+        /* soilSubmit.setOnClickListener {
+            Toast.makeText(this, "Select one soil type!", Toast.LENGTH_SHORT).show()
+        } */
+
+        // Season Data Receiving part
+        val receivedSeasonSelectionData = intent.getStringExtra("seasonData_key").toString()
+
+        /* demoText.text = receivedSeasonSelectionData */
+
+        soilSelectionRadioGrp.setOnCheckedChangeListener { _, i ->
+            val soilRadioButtonChecked = findViewById<RadioButton>(i)
+
+            val selectedSoilData = soilRadioButtonChecked.text.toString()
+
+//            val resultPageIntent = Intent(applicationContext, ResultPage::class.java)
+//            resultPageIntent.putExtra("seasonData_key", receivedSeasonSelectionData)
+//            resultPageIntent.putExtra("soilData_key", selectedSoilData)
+//            startActivity(resultPageIntent)
+
+            soilSelectionRadioGrp.clearCheck()
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/agrohold/SplashScreen.kt
@@ -0,0 +1,29 @@
+package com.example.healthyharvest
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.agrohold.MainActivity
+import com.google.android.material.snackbar.Snackbar
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500)
+
+        Snackbar.make(findViewById(R.id.splash_activity), "Harvesting Welcomes you ✨", Toast.LENGTH_SHORT)
+            .setBackgroundTint(resources.getColor(R.color.black))
+            .setTextColor(resources.getColor(R.color.white))
+            .show()
+    }
+}
